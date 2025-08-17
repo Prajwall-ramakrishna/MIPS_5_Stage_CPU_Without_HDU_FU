12c12c465e3460628ca7abdcccdb9ddb956ee846
Prajwall-ramakrishna <prajwalramakrishna@alumni.usc.edu>
2025-08-17 10:18:03 -0700

    Update alu.sv

--- alu.sv
+++ alu.sv
@@ -7,7 +7,7 @@ module alu(
   localparam ADD=4'b0010, SUB=4'b0110, AND=4'b0000, OR=4'b0001, SLT=4'b0111;
   always @(*) begin
     case (alu_ctrl)
-      ADD: y = a + b;
+      ADD: y = a + b;   //In MIPS, the sum size is 32 bits if a & b are greater numbers, resulting in 33 bits, then the numbers are divided into multiple registers
       SUB: y = a - b;
       AND: y = a & b;
       OR : y = a | b;
